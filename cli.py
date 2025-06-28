e635a9b17c9903f60519d7d40c7387bdafb6c8a0
Jordi Mas <jmas@softcatala.org>
2025-06-28 13:42:06 +0200

    prompt

--- cli.py
+++ cli.py
@@ -4,6 +4,7 @@ from transformers import AutoProcessor, AutoModelForImageTextToText
 from file import process_file
 import argparse
 import os
+from prompts import transcribe_prompt
 
 GEMMA_MODEL_ID = "google/gemma-3n-E2B-it"
 
@@ -15,37 +16,6 @@ model = AutoModelForImageTextToText.from_pretrained(
     GEMMA_MODEL_ID, torch_dtype="auto", device_map="cpu"
 )
 
-identify_lang_prompt = {
-    "type": "text",
-    "text": "Identify the languages of the audio and generate a JSON with the following fields\n"
-    "- language: a iso-639 language identified\n"
-    "- confidence level from 0 to 1 of the language identified\n",
-}
-
-
-transcribe_prompt_works = {
-    "type": "text",
-    "text": "You are given an audio recording in Catalan language with multiple speakers. Your task is to separate the speech segments by speaker and assign a unique speaker label to each segment. Output the start time, end time, and speaker ID for each segment."
-    "Format:\n"
-    "[start_time - end_time] Speaker_ID: Transcription (optional)\n"
-    "\n"
-    "Example:\n"
-    "[00:00:00 - 00:00:05] Speaker_1: Hello, how are you?\n"
-    "[00:00:06 - 00:00:10] Speaker_2: I'm good, thanks!\n"
-    "\n"
-    "Please provide the diarization results.\n",
-}
-
-transcribe_prompt = {
-    "type": "text",
-    "text": "You are given an audio recording in Catalan language with multiple speakers. Your task is to separate the speech segments by speaker and assign a unique speaker label to each segment. Output the start time, end time, and speaker ID for each segment and gender.",
-    # "Format:\n"
-    # "[start_time - end_time] Speaker_ID: Transcription\n"
-}
-
-
-# Does not seem to honor JSON
-transcribe_prompt = {"type": "text", "text": "Transcribe the audio file in Catalan.\n"}
 
 
 def transcribe_file(filename):
@@ -86,10 +56,13 @@ def main():
     args = parser.parse_args()
 
     results = transcribe_file(args.input_file)
-
+    
+    audio_basename = os.path.basename(args.input_file)
+    audio_basename = os.path.splitext(audio_basename)[0]
     output_path = os.path.join(
-        args.output_dir, os.path.basename(args.input_file) + ".txt"
-    )
+        self.output_dir, audio_basename + "." + "text"
+    )    
+
     with open(output_path, "w") as outfile:
         for result in results:
             outfile.write(result)
---
+++ prompts.py
@@ -0,0 +1,32 @@
+identify_lang_prompt = {
+    "type": "text",
+    "text": "Identify the languages of the audio and generate a JSON with the following fields\n"
+    "- language: a iso-639 language identified\n"
+    "- confidence level from 0 to 1 of the language identified\n",
+}
+
+
+transcribe_prompt_works = {
+    "type": "text",
+    "text": "You are given an audio recording in Catalan language with multiple speakers. Your task is to separate the speech segments by speaker and assign a unique speaker label to each segment. Output the start time, end time, and speaker ID for each segment."
+    "Format:\n"
+    "[start_time - end_time] Speaker_ID: Transcription (optional)\n"
+    "\n"
+    "Example:\n"
+    "[00:00:00 - 00:00:05] Speaker_1: Hello, how are you?\n"
+    "[00:00:06 - 00:00:10] Speaker_2: I'm good, thanks!\n"
+    "\n"
+    "Please provide the diarization results.\n",
+}
+
+transcribe_prompt = {
+    "type": "text",
+    "text": "You are given an audio recording in Catalan language with multiple speakers. Your task is to separate the speech segments by speaker and assign a unique speaker label to each segment. Output the start time, end time, and speaker ID for each segment and gender.",
+    # "Format:\n"
+    # "[start_time - end_time] Speaker_ID: Transcription\n"
+}
+
+
+# Does not seem to honor JSON
+transcribe_prompt = {"type": "text", "text": "Transcribe the audio file in Catalan.\n"}
+
